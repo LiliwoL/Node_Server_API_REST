f3ae2023a6aa2d0a8794ce7a7dec9bd1fcb08fd1
Serveur Virtuel pour la formation <formation@vps808653.ovh.net>
2020-06-17 14:32:22 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,93 @@
+/*
+	Server Node avec Express
+*/
+
+// Appeler le module express
+// Il faut avoir fait npm install express avant!
+const express = require('express');
+
+// Dépendance pour gérer les fichiers
+const fs = require("fs");
+
+// On va exécuter la fonction express() qui va instancier un objet express
+const app = express();
+
+// On demande à express d'utiliser le middleware JSON
+app.use(express.json());
+
+// Configurer une route
+// Qui répond à la méthode HTTP GET
+// et à l'url /
+app.get(
+	'/',
+	function (req, res) 
+	{
+		res.send('Hello World');
+	}
+);
+
+// Une route en GET avec un paramètre id
+app.get(
+	'/movies/:id',
+	function (req, res)
+	{
+		console.log(`## Requête sur le movie: ${req.params.id}`);
+
+		// Lecture du JSON
+		var data = fs.readFileSync("movies.json", 'utf8');
+
+		// Parsing JSON du contenu du fichier (qui était une string)
+		var json = JSON.parse(data);
+
+		// On veut le movie req.params.id
+		var output = json[req.params.id];
+
+		// Préparation de la réponse en JSON
+		res.setHeader('Content-Type', 'application/json');
+		res.send(output);
+	}
+);
+
+// Une route en POST avec l'url /movie/add
+app.post(
+	'/movie/add',
+	function (req, res)
+	{
+		console.log(`## Requête pour ajouter un film`);
+
+		console.log (req.body);
+
+		// Appel de la fonction pour l'ajout au fichier
+		var lastItem = addMovie( req.body );
+
+		console.log("OK, film ajouté");
+
+		// Redirection vers le film nouvellement ajouté
+		res.redirect('/movies/' + lastItem);
+	}
+)
+
+// On lance le serveur sur le port 3000
+app.listen(3000);
+
+
+// Fonction qui ajoute un film au fichier JSON du serveur
+function addMovie( newMovie )
+{
+	//@TODO: vérification du contenu!
+
+	// Lecture du JSON
+	var data = fs.readFileSync("movies.json", 'utf8');
+
+	// Parsing JSON du contenu du fichier (qui était une string)
+	var content = JSON.parse(data);
+
+	// Ajout du nouveau film au JSON
+	content.push( newMovie );
+
+	// Ecriture dans le fichier
+	fs.writeFileSync("movies.json", JSON.stringify(content) , 'utf8');
+
+	// On renvoie le dernier item du fichier (longueur -1)
+	return Object.keys(content).length -1;
+}
